977814ec7db5c8a5b7c31f7f305e24c74f15c322
koraboina <rajesh.koraboina@gmail.com>
2018-01-19 17:14:00 +0530

    Files Added

---
+++ no.py
@@ -0,0 +1,12 @@
+a=20
+b=10
+c=5
+d=30
+e=40
+if (e > b) and (e > c):
+    print("large=e")
+elif (d > e) and (b > e):
+    print("large=d")
+else:
+    print("no large")
+    print("The largest of the 5 numbers is : ", e,d,)

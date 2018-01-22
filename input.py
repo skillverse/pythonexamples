41fd1c38c8d0f51ffa50ed75e75c2e145961da35
skillverse <skillverseit@gmail.com>
2018-01-22 17:41:30 +0530

    New File Calender Generator

--- input.py
+++ input.py
@@ -1,2 +1,49 @@
 number=input("enter the month number")
-print("The number you have entered is "+str(number))
+if(number=='1'):
+    for date in range(31):
+        print(str(date))
+print("Printed to a range")
+if(number=='2'):
+    for date in range(28):
+        date+=1
+        print(str(date))
+if(number=='3'):
+    for date in range(31):
+        date+=1
+        print(str(date))
+if(number=='4'):
+    for date in range(30):
+        date+=1
+        print(str(date))
+if(number=='5'):
+    for date in range(31):
+        date+=1
+        print(str(date))
+if(number=='6'):
+    for date in range(30):
+        date+=1
+        print(str(date))
+if(number=='7'):
+    for date in range(31):
+        date+=1
+        print(str(date))
+if(number=='8'):
+    for date in range(31):
+        date+=1
+        print(str(date))
+if(number=='9'):
+    for date in range(30):
+        date+=1
+        print(str(date))
+if(number=='10'):
+    for date in range(31):
+        date+=1
+        print(str(date))
+if(number=='11'):
+    for date in range(30):
+        date+=1
+        print(str(date))
+if(number=='12'):
+    for date in range(31):
+        date+=1
+        print(str(date))
---
+++ input_new.py
@@ -0,0 +1,58 @@
+print('\n\nJanuary')
+for date in range(31):
+    print(str(date)+" ",end='')
+#month=
+print('\n\nFeburary')
+for date in range(28):
+    date+=1
+    print(str(date)+" ",end='')
+#month=
+print('\n\nMarch')
+for date in range(31):
+    date+=1
+    print(str(date)+" ",end='')
+#month=
+print('\n\nApril')
+for date in range(30):
+    date+=1
+    print(str(date)+" ",end='')
+#month=
+print('\n\nMay')
+for date in range(31):
+    date+=1
+    print(str(date)+" ",end='')
+#month=
+print('\n\nJune')
+for date in range(30):
+    date+=1
+    print(str(date)+" ",end='')
+#month=
+print('\n\nJuly')
+for date in range(31):
+    date+=1
+    print(str(date)+" ",end='')
+#month=
+print('\n\nAugust')
+for date in range(31):
+    date+=1
+    print(str(date)+" ",end='')
+#month=
+print('\n\nSeptember')
+for date in range(30):
+    date+=1
+print(str(date)+" ",end='')
+#month=
+print('\n\nOctober')
+for date in range(31):
+    date+=1
+    print(str(date)+" ",end='')
+#month=
+print('\n\nNovember')
+for date in range(30):
+    date+=1
+    print(str(date)+" ",end='')
+#month=
+print('\n\nDecember')
+for date in range(31):
+    date+=1
+    print(str(date)+" ",end='')

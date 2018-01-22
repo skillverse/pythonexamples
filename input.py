1eee08ff5abf034f7df2b29a1a4586030f9130d6
koraboina <rajesh.koraboina@gmail.com>
2018-01-22 17:10:02 +0530

    dates

--- input.py
+++ input.py
@@ -1,4 +1,25 @@
-for i in range(100):
-         number=input("enter the month number")
-          break
-print("The number you have entered is "+str(number))
+for i in range(31):
+             print("date:"+str(i))
+for i in range(28):
+             print("date:"+str(i))
+for i in range(31):
+             print("date:"+str(i))
+for i in range(30):
+             print("date:"+str(i))
+for i in range(31):
+             print("date:"+str(i))
+for i in range(30):
+             print("date:"+str(i))
+for i in range(31):
+             print("date:"+str(i))
+for i in range(31):
+             print("date:"+str(i))
+for i in range(30):
+             print("date:"+str(i))
+for i in range(31):
+             print("date:"+str(i))
+for i in range(30):
+             print("date:"+str(i))
+for i in range(31):
+             print("date:"+str(i))
+            

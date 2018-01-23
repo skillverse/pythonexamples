3fd07116ff143f2ce9d3503fcfaab1333f32bbb5
skillverse <skillverseit@gmail.com>
2018-01-23 16:32:21 +0530

    Calender Generator

--- input_new.py
+++ input_new.py
@@ -4,64 +4,89 @@ print('\n\nJanuary')
 for date in range(31):
     date+=1
     print(str(date)+" ",end='')
+    if date%7==0:
+        print("\n")
 #month=
 print('\n\nFeburary')
 if(year%4==0):
     for date in range(29):
         date+=1
         print(str(date)+" ",end='')
+        if date%7==0:
+            print("\n")
 else:
     for date in range(28):
         date+=1
         print(str(date)+" ",end='')
-
+        if date%7==0:
+            print("\n")
 #month=
 print('\n\nMarch')
 for date in range(31):
     date+=1
     print(str(date)+" ",end='')
+    if date%7==0:
+        print("\n")
 #month=
 print('\n\nApril')
 for date in range(30):
     date+=1
     print(str(date)+" ",end='')
+    if date%7==0:
+        print("\n")
 #month=
 print('\n\nMay')
 for date in range(31):
     date+=1
     print(str(date)+" ",end='')
+    if date%7==0:
+        print("\n")
 #month=
 print('\n\nJune')
 for date in range(30):
     date+=1
     print(str(date)+" ",end='')
+    if date%7==0:
+        print("\n")
 #month=
 print('\n\nJuly')
 for date in range(31):
     date+=1
     print(str(date)+" ",end='')
+    if date%7==0:
+        print("\n")
 #month=
 print('\n\nAugust')
 for date in range(31):
     date+=1
     print(str(date)+" ",end='')
+    if date%7==0:
+        print("\n")
 #month=
 print('\n\nSeptember')
 for date in range(30):
     date+=1
 print(str(date)+" ",end='')
+if date%7==0:
+    print("\n")
 #month=
 print('\n\nOctober')
 for date in range(31):
     date+=1
     print(str(date)+" ",end='')
+    if date%7==0:
+        print("\n")
 #month=
 print('\n\nNovember')
 for date in range(30):
     date+=1
     print(str(date)+" ",end='')
+    if date%7==0:
+        print("\n")
 #month=
 print('\n\nDecember')
 for date in range(31):
     date+=1
     print(str(date)+" ",end='')
+    if date%7==0:
+        print("\n")

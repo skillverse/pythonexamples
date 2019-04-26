34f3a94691dc9a3fd4a6127cd1e2aab9452cf7a1
Giridher Sadineni <giridher@hotmail.com>
2019-04-26 08:35:47 +0530

    done changes

--- exl.py
+++ exl.py
@@ -1,6 +1,7 @@
 for letter in 'Python':
            print ("Current Letter :", letter)
-           fruits = ['banana', 'apple',  'mango']
+           
+fruits = ['banana', 'apple',  'mango']
 for fruit in fruits:
            print ('Current fruit :', fruit)
 print ("Good bye!")
--- exm.py
+++ exm.py
@@ -4,9 +4,9 @@ c=1
 d=20
 e=15
 if a<b:
-    print("a is  grater then b")
+    print("a is  greater then b")
 elif b<c:
-print("b  is greater then a")
+    print("b  is greater then a")
 elif c<d:
     print("c  is greater then b")
 elif d<e:
